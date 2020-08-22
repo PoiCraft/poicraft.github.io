319c94312c5ba05071a1e6bccd1849b63af6fa30
gggxbbb <2331490629@qq.com>
2020-08-22 16:27:02 +0800

    add sth.

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -12,14 +12,24 @@ module.exports = {
     },
     themeConfig: {
         sidebarDepth: 2,
+        repo: 'PoiCraft/PoiCraft_Open',
+        smoothScroll: true,
         locales: {
             '/': {
                 selectText: '选择语言',
                 label: '简体中文',
                 editLinkText: '在 GitHub 上编辑此页',
+                repoLabel: '查看源码',
                 lastUpdated: '最后更新于',
                 sidebar: [
                     ['/', '主页'],
+                    {
+                        title: '使用的项目',
+                        children: [
+                            ['/depend/open', '使用的开源项目'],
+                            ['/depend/close', '使用的闭源软件']
+                        ]
+                    }
                 ]
             },
         },
