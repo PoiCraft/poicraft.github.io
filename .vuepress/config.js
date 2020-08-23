61a1de47f067fddec530b2714ecd8d99aa794618
gggxbbb <2331490629@qq.com>
2020-08-23 10:55:56 +0800

    remove vuepress-theme-antdocs

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,35 +1,43 @@
 module.exports = {
     title: 'PoiCraft',
-    description: 'PoiCraft 开源计划',
-    lang: 'zh-cn',
+    description: 'PoiCraft Open Source Project',
     markdown: {
         lineNumbers: true
     },
-    theme: 'antdocs',
+    locales: {
+        '/': {
+            lang: 'zh-cn',
+            description: 'PoiCraft 开源计划'
+        },
+    },
     themeConfig: {
         sidebarDepth: 2,
         repo: 'PoiCraft/PoiCraft_Open',
         editLinks: true,
         smoothScroll: true,
-        selectText: '选择语言',
-        label: '简体中文',
-        editLinkText: '在 GitHub 上编辑此页',
-        repoLabel: '查看源码',
-        nav: [
-            { text: '大事记', link: '/timeline' },
-            { text: '相关项目', link: '/prop/' }
-        ],
-        sidebar: {
-            '/prop/': [
-                ['/prop/', '概述'],
-                {
-                    title: '使用的项目',
-                    children: [
-                        ['/prop/depend/open', '使用的开源项目'],
-                        ['/prop/depend/close', '使用的闭源软件']
+        locales: {
+            '/': {
+                selectText: '选择语言',
+                label: '简体中文',
+                editLinkText: '在 GitHub 上编辑此页',
+                repoLabel: '查看源码',
+                nav: [
+                    { text: '大事记', link: '/timeline' },
+                    { text: '相关项目', link: '/prop/' }
+                ],
+                sidebar: {
+                    '/prop/': [
+                        ['/prop/', '概述'],
+                        {
+                            title: '使用的项目',
+                            children: [
+                                ['/prop/depend/open', '使用的开源项目'],
+                                ['/prop/depend/close', '使用的闭源软件']
+                            ]
+                        }
                     ]
                 }
-            ]
+            },
         },
 
     }
